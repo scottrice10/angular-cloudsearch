20094934032ce7be1ddb20cec4824aeb050264d4
Scott Rice <scottrice10@gmail.com>
2015-01-04 23:09:00 -0800

    Integration with Amazon Cloudsearch.

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,47 +3,105 @@
   'use strict';
   var express = require('express');
   var router = express.Router();
-  var mongojs = require('mongojs');
-  var db = mongojs('meanTodo', ['todos']);
+  var AWS = require('aws-sdk');
 
-  /* GET home page. */
+  //AWS configuration
+  AWS.config.loadFromPath('./config.json');
+  var cloudsearchdomain = new AWS.CloudSearchDomain({
+    endpoint: 'http://doc-imorgo-o7j3rjbk2ekyfkupbetbyfjoym.us-west-2.cloudsearch.amazonaws.com',
+    apiVersion: '2013-01-01'
+  });
+
+  // GET home page.
   router.get('/', function(req, res) {
     res.render('index');
   });
 
-  router.get('/api/todos', function(req, res) {
-    db.todos.find(function(err, data) {
-      res.json(data);
+  //Search api get request can take the following parameters:
+  //http://imorgo.com/api/search?
+  // query={searchTerm}&
+  // facet={[facetName1, facetName2]}&
+  // size={numberResultsReturnedPerRequest}&
+  // page={pageNumber}
+  router.get('/api/search', function(req, res) {
+    var params = {
+      query: req.query.query ? req.query.query + "*" : "",
+      facet: function buildFacet() {
+        var facets = req.query.facets;
+        var facetsString = "{";
+        for(var i = 0; i < facets.length; i++) {
+          facetsString += '"' + facets[i] + '":{"sort":"bucket", "size":10}';
+
+          if(i !== facets.length - 1) {
+            facetsString += ","
+          }
+        }
+        facetsString += "}";
+
+        return facetsString;
+      }(),
+      partial: true,
+      queryOptions: '{"defaultOperator": "or"}',
+      queryParser: 'simple',
+      return: 'all_fields',
+      size: req.query.size || 10,
+      sort: 'score desc',
+      start: (req.query.page || 0) * this.size
+    };
+
+    cloudsearchdomain.search(params, function(err, data) {
+      if(err) {
+        console.log(err, err.stack);
+      } else {
+        res.json(data);
+      }
     });
   });
 
-  router.post('/api/todos', function(req, res) {
-    db.todos.insert(req.body, function(err, data) {
-      res.json(data);
-    });
+  //Search api post request takes only post object, which must be an array of properly formatted json objects
+  //** Note that an existing cloudsearch document cannot technically be updated. When a document with an existing id is
+  //uploaded, that document overwrites the document with that same id: http://docs.aws.amazon.com/cloudsearch/latest/developerguide/preparing-data.html.
+  //** Also note that limit for http post data is 2GB: http://stackoverflow.com/questions/2880722/is-http-post-limitless.
+  //http://imorgo.com/api/search?
+  router.post('/api/search', function(req, res) {
+    var params = {
+      contentType: 'application/json',
+      documents: JSON.stringify(req.body)
+    };
 
+    cloudsearchdomain.uploadDocuments(params, function(err, data) {
+      if(err) {
+        console.log(err, err.stack);
+      }
+      else {
+        res.json(data);
+      }
+    });
   });
 
-  router.put('/api/todos', function(req, res) {
+  //Search api delete request takes the following parameters:
+  //http://imorgo.com/api/search?
+  // ids={[id1, id2]}
+  router.delete('/api/search', function(req, res) {
+    var docsToDelete = [];
+    for(var i = 0; i < req.query.ids.length; i++) {
+      var id = req.query.ids[i];
+      docsToDelete.push({"type": "delete", "id": id});
+    }
 
-    db.todos.update({
-      _id: mongojs.ObjectId(req.body._id)
-    }, {
-      isCompleted: req.body.isCompleted,
-      todo: req.body.todo
-    }, {}, function(err, data) {
-      res.json(data);
+    var params = {
+      contentType: 'application/json',
+      documents: JSON.stringify(docsToDelete)
+    };
+
+    cloudsearchdomain.uploadDocuments(params, function(err, data) {
+      if(err) {
+        console.log(err, err.stack);
+      }
+      else {
+        res.json(data);
+      }
     });
-
-  });
-
-  router.delete('/api/todos/:_id', function(req, res) {
-    db.todos.remove({
-      _id: mongojs.ObjectId(req.params._id)
-    }, '', function(err, data) {
-      res.json(data);
-    });
-
   });
 
   module.exports = router;
